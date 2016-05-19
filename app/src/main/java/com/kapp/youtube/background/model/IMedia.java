9b2aef208de11171526b9ab3815d1eb1228848e5
Khang <khang.neon.1997@gmail.com>
2016-05-19 12:35:23 +0700

    Add MediaItem view

---
+++ app/src/main/java/com/kapp/youtube/background/model/IMedia.java
@@ -0,0 +1,21 @@
+package com.kapp.youtube.background.model;
+
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by khang on 19/05/2016.
+ * Email: khang.neon.1997@gmail.com
+ */
+public interface IMedia {
+    String getTitle();
+
+    String getDescription();
+
+    String getDuration();
+
+    int getCardColor();
+
+    void bindThumbnail(ImageView imageView, Picasso picasso);
+}
